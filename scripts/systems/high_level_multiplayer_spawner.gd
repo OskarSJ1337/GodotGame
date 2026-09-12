4c7617431ac3da9e071a3a77b20c3f5ca4f61efe
Oskar <OskarSJ1337@users.noreply.github.com>
2026-09-12 22:40:03 +0200

    Multiplayer seems to work

--- scripts/systems/high_level_multiplayer_spawner.gd
+++ scripts/systems/high_level_multiplayer_spawner.gd
@@ -1,15 +1,18 @@
 extends MultiplayerSpawner
 
- 
 @export var network_player : PackedScene
+@export var spawn_position := Vector2(400, 320)
 
 func _ready() -> void:
+	spawn_function = Callable(self, "_spawn_player")
 	multiplayer.peer_connected.connect(spawn_player)
 	
 func spawn_player(id: int) -> void:
 	if !multiplayer.is_server(): return
-	
+	spawn({"id": id, "position": spawn_position})
+
+func _spawn_player(data: Dictionary) -> Node:
 	var player: Node = network_player.instantiate()
-	player.name = str(id)
-	
-	get_node(spawn_path).call_deferred("add_child", player)
+	player.name = str(data["id"])
+	player.position = data["position"]
+	return player
