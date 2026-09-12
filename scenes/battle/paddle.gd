4633aa009724b680198a3fab9ccc758ae1c89c2e
Oskar <OskarSJ1337@users.noreply.github.com>
2026-09-12 22:33:31 +0200

    Changed paddle to include netcoding

--- scenes/battle/paddle.gd
+++ scenes/battle/paddle.gd
@@ -3,11 +3,16 @@ extends CharacterBody2D
 const speed := 800.0
 
 
+func _enter_tree() -> void:
+	set_multiplayer_authority(name.to_int())
+
 func getYDir() -> float:
 	return Input.get_action_strength("down") - Input.get_action_strength("up")
 
 
 func _physics_process(delta: float) -> void:
+	if !is_multiplayer_authority(): return
+	
 	var base_dir = Vector2(0,getYDir())
 	var dir: Vector2=base_dir.rotated(deg_to_rad(rotation_degrees))
 	velocity = dir * speed
