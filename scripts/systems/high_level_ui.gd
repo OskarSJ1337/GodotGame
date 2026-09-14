33db6f95e1f050c01193b8bc738e6017aa064ac1
Oskar <OskarSJ1337@users.noreply.github.com>
2026-09-14 17:15:27 +0200

    Trying to print IP and port

--- scripts/systems/high_level_ui.gd
+++ scripts/systems/high_level_ui.gd
@@ -4,14 +4,14 @@ extends Control
 @onready var port_input: LineEdit = $"../PortInput"
 
 func _on_server_pressed():
+	var ip := ip_input.text.strip_edges()
 	var port := int(port_input.text)
-	print(port) 
-	HighLevelNetworkHandler.start_server()
+	print("IP: ", ip)
+	print("Port: ", port)
 
 
 func _on_client_pressed():
 	var ip := ip_input.text.strip_edges()
 	var port := int(port_input.text)
-	print(ip)
-	print(port) 
-	HighLevelNetworkHandler.start_client()
+	print("IP: ", ip)
+	print("Port: ", port)
