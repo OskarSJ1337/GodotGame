a7de01145918f1fbf84c83432b97edfc605cedf2
Oskar <OskarSJ1337@users.noreply.github.com>
2026-09-15 21:29:02 +0200

    Borders, and 4 players working as intended now

--- scripts/systems/high_level_multiplayer_spawner.gd
+++ scripts/systems/high_level_multiplayer_spawner.gd
@@ -1,9 +1,20 @@
 extends MultiplayerSpawner
 
 @export var network_player : PackedScene
-const SPAWN_POSITIONS:= [Vector2(384,488), Vector2(792,112)]
-#const PADDLE_COLORS:= [Color.REBECCA_PURPLE, Color.BLUE]
-const PADDLE_ROTATION:= [315.0, 135.0]
+const SPAWN_POSITIONS:= [
+	Vector2(384,488), 
+	Vector2(760,144), 
+	Vector2(416,144), 
+	Vector2(752,480)
+	]
+
+const PADDLE_ROTATION:= [
+	315.0, 
+	135.0, 
+	45.0, 
+	45.0
+	]
+	
 var spawn_order := 0
 
 func _ready() -> void:
@@ -19,25 +30,28 @@ func _spawn_host_player() -> void:
 		spawn_player(1)
 	
 func spawn_player(id: int) -> void:
-	if !multiplayer.is_server(): return
+	if !multiplayer.is_server():
+		return
 	spawn({
 		"id": id,
 		"position": SPAWN_POSITIONS[spawn_order],
 		"color": Color.from_hsv(randf(), 0.8, 1.0),
-		"rotation": PADDLE_ROTATION[spawn_order]
+		"rotation": PADDLE_ROTATION[spawn_order],
+		"spawn_order": spawn_order
 	})
 	spawn_order += 1
-	
-	if spawn_order < 3:
-		$"../Borders/2p border1".visible = true
-		$"../Borders/2p border2".visible = true
-
-
 
 func _spawn_player(data: Dictionary) -> Node:
 	var player: Node = network_player.instantiate()
+
 	player.name = str(data["id"])
 	player.position = data["position"]
 	player.modulate = data["color"]
 	player.rotation_degrees = data["rotation"]
+
+	if data["spawn_order"] == 2:
+		$"../Borders/2p border1".queue_free()
+	if data["spawn_order"] == 3:
+		$"../Borders/2p border2".queue_free()
+
 	return player
