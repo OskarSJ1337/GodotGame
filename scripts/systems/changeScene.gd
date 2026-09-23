011bcb9d0a552e334f6787b72ff1024ece7c2cb5
Erik <erik_risberg@hotmail.com>
2026-09-23 12:14:40 +0200

    added some system sounds

--- scripts/systems/changeScene.gd
+++ scripts/systems/changeScene.gd
@@ -2,6 +2,14 @@ extends Button
 
 @export var next_scene: PackedScene
 
+func _ready() -> void:
+	mouse_entered.connect(_on_mouse_entered)
+	
+func _on_mouse_entered() -> void:
+	UiSounds.hover.play()
+
 func _pressed() -> void:
+	UiSounds.click.play()
 	if next_scene:
+		await UiSounds.click.finished
 		get_tree().change_scene_to_packed(next_scene)
