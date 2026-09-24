6e42ac98b84d8984429269e3fd378c3a00519103
Oskar <OskarSJ1337@users.noreply.github.com>
2026-09-24 11:29:50 +0200

    Added so the rotation is toggleable

--- scripts/components/rotating_borders.gd
+++ scripts/components/rotating_borders.gd
@@ -3,12 +3,14 @@ extends Node2D
 
 @export var rotation_speed: float = 10.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var is_rotating: bool = false
 
+func _ready() -> void:
+	$"../VBoxContainer/Rotate".pressed.connect(_on_rotate_pressed)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	rotation += deg_to_rad(rotation_speed) * delta
-	#$"../VBoxContainer/Rotate"
+func _on_rotate_pressed() -> void:
+	is_rotating = not is_rotating
+
+func _process(delta: float) -> void:
+	if is_rotating:
+		rotation += deg_to_rad(rotation_speed) * delta
